fdc78ce9b8eeb148f2f6378abf18c1e8aa7f37f3
Tom F. <tom.fucik@email.cz>
2026-09-20 15:41:23 +0200

    feat(app): add app base

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:bakalite/api.dart';
+import 'package:bakalite/theme.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BakaLite',
+      debugShowCheckedModeBanner: false,
+
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+    );
+  }
+}
+
