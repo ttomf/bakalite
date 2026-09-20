5c878db414b1155a1dbc5e4676def323944e64d2
Tom F. <tom.fucik@email.cz>
2026-09-20 14:27:27 +0200

    feat(api): add municipality endpoint

---
+++ lib/api.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+class BakalariAPI {
+  static const mainBaseUrl = 'https://sluzby.bakalari.cz/api/v1';
+
+  Future<Map<String, int>> getSchools() async {
+    try {
+      final res = await http.get(
+        Uri.parse('$mainBaseUrl/municipality'),
+        headers: {'Accept': 'application/json'},
+      );
+
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        throw HttpException('HTTP ${res.statusCode}');
+      }
+
+      final json = jsonDecode(res.body);
+
+      if (json is! List) {
+        throw const FormatException('Expected a JSON list');
+      }
+
+      final map = <String, int>{};
+
+      for (final city in json) {
+        if (city is! Map<String, dynamic>) {
+          throw const FormatException('Expected a list of JSON objects');
+        }
+
+        map[city['name']] = city['schoolCount'];
+      }
+
+      return map;
+    } on http.ClientException {
+      throw Exception('Network error');
+    }
+  }
+
+  Future<Map<String, String>> getSchoolsIn(String city) async {
+    try {
+      final res = await http.get(
+        Uri.parse('$mainBaseUrl/municipality/$city'),
+        headers: {'Accept': 'application/json'},
+      );
+
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        throw HttpException('HTTP ${res.statusCode}');
+      }
+
+      final json = jsonDecode(res.body);
+
+      if (json is! Map) {
+        throw const FormatException('Expected a JSON dict');
+      }
+
+      if (json["schools"] is! List) {
+        throw const FormatException('Expected a JSON list');
+      }
+
+      final map = <String, String>{};
+
+      for (final school in json["schools"]) {
+        if (school is! Map<String, dynamic>) {
+          throw const FormatException('Expected a list of JSON objects');
+        }
+
+        map[school['name']] = school['schoolUrl'];
+      }
+
+      return map;
+    } on http.ClientException {
+      throw Exception('Network error');
+    }
+  }
+}
