b37853fac895a1b5de9aa1e87d8986d19411c621
Tom F. <tom.fucik@email.cz>
2026-09-26 16:06:52 +0200

    style: better theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData get lightTheme {
+  static ThemeData getThemeData(Brightness brightness) {
+    final colorScheme = ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: brightness,
+    );
+
     return ThemeData(
       useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue,
-        brightness: Brightness.light,
+      scaffoldBackgroundColor: colorScheme.surface,
+      dialogTheme: DialogThemeData(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        backgroundColor: colorScheme.surface,
       ),
+      cardTheme: CardThemeData(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        clipBehavior: Clip.antiAlias,
+      ),
+      inputDecorationTheme: InputDecorationThemeData(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      listTileTheme: ListTileThemeData(
+        contentPadding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+      ),
+      colorScheme: colorScheme,
     );
   }
 
+  static ThemeData get lightTheme {
+    return getThemeData(Brightness.light);
+  }
+
   static ThemeData get darkTheme {
-    return ThemeData(
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue,
-        brightness: Brightness.dark,
-      ),
-    );
+    return getThemeData(Brightness.dark);
   }
 }
